b701865cdf9be11890364d36a79aba07464601f8
blebar25 <56796185+blebar25-lab@users.noreply.github.com>
2025-04-06 23:16:46 +0300

    Add SVG favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#2C3E50" />
-        <meta name="msapplication-TileColor" content="#2C3E50" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+        <meta name="theme-color" content="#2C3E50" />
       </Head>
       <body>
         <Main />
